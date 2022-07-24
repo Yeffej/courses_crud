2cfc9c6a25e961687b225494c198353f67a608a9
Yefri Encarnación <yefriej128@gmail.com>
2022-07-24 16:54:17 -0400

    setting header allowing access from: *

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const courseRouter = require("./course")
+const setHeadersConfig = require("../middlewares/setHeadersConfig")
 
 const router = express.Router()
 
+router.use(setHeadersConfig)
 router.get("/", (req, res) => {
     res.send({hello: "world"})
 })
